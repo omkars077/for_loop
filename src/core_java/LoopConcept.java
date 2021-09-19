1898000e4e3688d599be8f3e55ac11475caa0cbe
Omkar Singh <omkars077@gmail.com>
2021-09-19 12:05:11 -0700

    advanced forloop

--- src/core_java/LoopConcept.java
+++ src/core_java/LoopConcept.java
@@ -4,19 +4,17 @@ public class LoopConcept {
 
     public static void main(String[] args){
 
-        for (int i=0; i<7; i++){
-            System.out.println("It is used for  repeating any statement : "+i);
-        }
-        for (int i=5; i>0; i--){
-            System.out.println("it shows values from down to up and called decrement method : "+i);
+        for (int i=1; i<11; i++){
+            int five=5;
+            int result=five*i;
+            System.out.println(result+" : "+i);
         }
         int i=0;
-        while (i<6){
+        while (i<10){
             i++;
-            System.out.println("This is while loop : "+i);
+            int eight=8;
+            int output=eight*i;
+            System.out.println(output);
         }
-
-
     }
-
 }
