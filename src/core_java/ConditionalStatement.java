7b05be222115a24fef92213a8f17c6b52d9e8df7
Omkar Singh <omkars077@gmail.com>
2021-09-19 12:46:11 -0700

    if and else

---
+++ src/core_java/ConditionalStatement.java
@@ -0,0 +1,24 @@
+package core_java;
+
+public class ConditionalStatement {
+
+    public static void main(String[] args){
+
+        int age=22;
+        if (age<18){
+            System.out.println("Person will be called adult.");
+        }
+        else {
+            System.out.println("Person will be called adolescence.");
+        }
+        String brand="Dell";
+        if (brand=="Lenovo"){
+            System.out.println("This is real product.");
+        }
+        else {
+            System.out.println("This is wrong product.");
+        }
+    }
+
+
+}
