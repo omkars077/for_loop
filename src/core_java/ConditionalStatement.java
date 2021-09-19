cd05feb466d20ce529e95455891308d60a756f24
Omkar Singh <omkars077@gmail.com>
2021-09-19 13:29:05 -0700

    if and else1

--- src/core_java/ConditionalStatement.java
+++ src/core_java/ConditionalStatement.java
@@ -3,21 +3,33 @@ package core_java;
 public class ConditionalStatement {
 
     public static void main(String[] args){
+        String post="Developer";
+        int age=25;
+        if (age<22 && post=="Developer"){
+            System.out.println("This is right information");
+        }
+        else {
+            System.out.println("THIS IS WRONG REPORT.");
+        }
+        String brand="Redmi";
+        int price=12000;
+        if (price>16000 || brand=="Redmi"){
+            System.out.println("This is not high price .");
 
-        int age=22;
-        if (age<18){
-            System.out.println("Person will be called adult.");
+            }
+        else {
+            System.out.println("THIS IS SO HIGH PRICE PRODUCT AT THIS RATE.");
+        }
+
+        int salary=150000;
+        if (!(salary>120000)){
+            System.out.println("This is highest salary in this department.");
         }
         else {
-            System.out.println("Person will be called adolescence.");
+            System.out.println("This is not highest salary in this department.");
         }
-        String brand="Dell";
-        if (brand=="Lenovo"){
-            System.out.println("This is real product.");
-        }
-        else {
-            System.out.println("This is wrong product.");
-        }
+
+
     }
 
 
