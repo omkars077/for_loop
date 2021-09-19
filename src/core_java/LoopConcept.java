bd188c18c0112633bcf16771b86bebb89f34043f
Omkar Singh <omkars077@gmail.com>
2021-09-19 09:17:06 -0700

    Initial commit

---
+++ src/core_java/LoopConcept.java
@@ -0,0 +1,22 @@
+package core_java;
+
+public class LoopConcept {
+
+    public static void main(String[] args){
+
+        for (int i=0; i<7; i++){
+            System.out.println("It is used for  repeating any statement : "+i);
+        }
+        for (int i=5; i>0; i--){
+            System.out.println("it shows values from down to up and called decrement method : "+i);
+        }
+        int i=0;
+        while (i<6){
+            i++;
+            System.out.println("This is while loop : "+i);
+        }
+
+
+    }
+
+}
